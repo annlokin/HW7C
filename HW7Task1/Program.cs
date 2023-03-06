3cf0ea1c44bffe7ff702fb6e40d053f8aca4c212
Anna Nikolenko <nikolenko_anmai@mail.ru>
2023-03-06 21:33:23 +0300

    Task 1

---
+++ HW7Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double [,] GenerateArray(int row, int col) 
+{ 
+    double [,] array = new double [row, col];
+    Random random = new Random(); 
+    for (int i = 0; i < array.GetLength(0); i++) 
+    { 
+        for (int j = 0; j < array.GetLength(1); j++) 
+        { 
+            array[i, j] = Math.Round(random.Next(-10, 10)*0.1,1); 
+        } 
+    } 
+    return array; 
+} 
+
+void PrintArray(double [,] array) 
+{ 
+    for (int i = 0; i < array.GetLength(0); i++) 
+    { 
+        Console.WriteLine(); 
+        for (int j = 0; j < array.GetLength(1); j++) 
+        { 
+            Console.Write($"{array[i, j]}\t"); 
+        } 
+    } 
+    Console.WriteLine(); 
+}
+
+
+double [,] matrix = GenerateArray(3, 4); 
+PrintArray(matrix); 
+
