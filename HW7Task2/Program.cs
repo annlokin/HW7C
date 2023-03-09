030a285b9c7841b49d87c1b27866b3e6d2ff1073
Anna Nikolenko <nikolenko_anmai@mail.ru>
2023-03-10 00:27:28 +0300

    Task 2 and Task 3

---
+++ HW7Task2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+int InputUser(string message)
+{
+    System.Console.Write($"{message} = ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = (random.Next(1, 10));
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+bool SearchPosition(int[,] array, int row, int col)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i, col] == row)
+            return true;
+    }
+    System.Console.WriteLine("Нет такого числа в массиве");
+    return false;
+}
+
+int userRow = InputUser("Введите количество строк");
+int userColumn = InputUser("Введите количество столбцов");
+int[,] matrix = GenerateArray(userRow, userColumn);
+PrintArray(matrix);
+int searchRow = InputUser("Введите номер позиции строки");
+int searchColumn = InputUser("Введите номер позиции столбца");
+System.Console.WriteLine($"Элемент c позицией [{searchRow},{searchColumn}] = {SearchPosition(matrix, searchRow, searchColumn)}");
---
+++ HW7Task3/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int InputUser(string message)
+{
+    System.Console.Write($"{message} = ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = (random.Next(1, 10));
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+void AverageColArray(int[,] array)
+{
+    System.Console.WriteLine();
+    for (int j = 0; j < array.GetLength(1); j++)
+{
+    double average = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        average += array[i, j];
+    }
+System.Console.WriteLine($"В колонке {j + 1} = {Math.Round(average / array.GetLength(0),2)}");
+}
+}
+
+int userRow = InputUser("Введите количество строк");
+int userColumn = InputUser("Введите количество столбцов");
+int[,] matrix = GenerateArray(userRow, userColumn);
+PrintArray(matrix);
+Console.WriteLine("Среднее арифметическое каждого столбца = ");
+AverageColArray(matrix);
